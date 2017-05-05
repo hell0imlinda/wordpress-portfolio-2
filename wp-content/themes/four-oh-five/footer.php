d2f0320111bb4c2781b266c54650f9601e66c31b
hell0imlinda <linda.ov.600@my.csun.edu>
2017-05-04 19:34:07 -0700

    Initial Commit

--- wp-content/themes/four-oh-five/footer.php
+++ wp-content/themes/four-oh-five/footer.php
@@ -22,18 +22,21 @@
 		<p>Copyright &copy; <?php echo date("Y"); ?></p>
 	</div>
 </footer>
-<?php wp_footer(); ?>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 <script type="text/javascript">
 	// Can also be used with $(document).ready()
-$(window).load(function() {
-  $('.flexslider').flexslider({
-    animation: "slide"
-  });
-});
+	(function($){
+		$(window).load(function() {
+		  $('.flexslider').flexslider({
+		    animation: "slide"
+		  });
+		});
+	})(jQuery);
 </script>
+<?php wp_footer(); ?>
 </body>
 </html>
